8831c3cf36d20bbf40baf366e4b6dd6174190027
XRenso <tch.xr@yandex.ru>
2022-01-21 08:43:21 +1100

    Changing version to python 3.10

--- Text_RPg.py
+++ Text_RPg.py
@@ -1,6 +1,6 @@
 import random
 import os
-import Enemy_config as en_conf
+import Enemy_config as En_conf
 
 last_action_player = 'Пока ничего не произошло'
 
@@ -8,14 +8,14 @@ last_action_monster = 'Пока ничего не произошло'
 
 
 class Enemy(object):
-    def __init__(self, name, age, race, hp, MaxAttack, country):
+    def __init__(self, name, age, race, hp, max_attack, country):
         self.name = name
         self.age = age
         self.chance = random.randrange(1, 101)
         self.race = race
         self.hp = hp
         self.maxHP = hp
-        self.maxAttack = MaxAttack
+        self.maxAttack = max_attack
         self.country = country
 
     def get_damage(self, damage):
@@ -39,14 +39,14 @@ class Enemy(object):
     def health(self):
 
         if self.hp < self.maxHP:
-            can_u_heal = random.randrange(-5,self.chance + 1)
+            can_u_heal = random.randrange(-5, self.chance + 1)
             if self.chance != 0:
-               can_u_heal = round(can_u_heal/self.chance)
+                can_u_heal = round(can_u_heal / self.chance)
             if self.chance == 100:
                 heal = random.randrange(1, self.maxHP - self.hp)
                 self.hp += heal
                 self.chance = 0
-                return  f"Вот как бывает. {self.name} увеличило свое здоровье на {heal}"
+                return f"Вот как бывает. {self.name} увеличило свое здоровье на {heal}"
 
             elif self.chance > 0 and self.chance != 100 and can_u_heal >= 0.5:
                 heal = random.randrange(1, self.maxHP - self.hp)
@@ -54,35 +54,35 @@ class Enemy(object):
                 self.chance = 0
                 return f"Вот эта фортуна\nне для тебя..... \n{self.name} повысило хп на {heal}"
             elif can_u_heal < 0:
-                damage = random.randrange(1, self.maxHP-self.hp)
+                damage = random.randrange(1, self.maxHP - self.hp)
                 self.hp -= damage
-                self.chance += random.randrange(5,21)
-                return  f"О ДАААА. \n{self.name} поулчило чертов критический промах. \nШприц порвал его вены. \nОНо полуил {damage} урона"
+                self.chance += random.randrange(5, 21)
+                return f"О ДАААА. \n{self.name} поулчило чертов критический промах. \nШприц порвал его вены. \nОНо полуил {damage} урона"
 
             else:
-                self.chance += random.randrange(1,21)
-                return  'Сегодня не твой день. \nЯ думаю в следущий раз повезет'
-
+                self.chance += random.randrange(1, 21)
+                return 'Сегодня не твой день. \nЯ думаю в следущий раз повезет'
 
     def get_stat(self, whatUwant):
-        if whatUwant == 'name':
-            return self.name
-        elif whatUwant == 'age':
-            return self.age
-        elif whatUwant == 'country':
-            return self.country
-        elif whatUwant == 'hp':
-            return self.hp
-        elif whatUwant == 'race':
-            return self.race
-        elif whatUwant == 'maxHP':
-            return  self.maxHP
+        match whatUwant:
+            case 'name':
+                return self.name
+            case 'age':
+                return self.age
+            case 'country':
+                return self.country
+            case 'hp':
+                return self.hp
+            case 'race':
+                return self.race
+            case 'maxHP':
+                return self.maxHP
 
 
 class Player(object):
 
     def __init__(self, name, age, race, hp, MaxAttack, country):
-        self.chance = random.randrange(0,101)
+        self.chance = random.randrange(0, 101)
         self.surrender = False
         self.name = name
         self.age = age
@@ -109,14 +109,14 @@ class Player(object):
 
     def health(self):
         if self.hp < self.maxHP:
-            can_u_heal = random.randrange(-5,self.chance + 1)
+            can_u_heal = random.randrange(-5, self.chance + 1)
             if self.chance != 0:
-               can_u_heal = round(can_u_heal/self.chance)
+                can_u_heal = round(can_u_heal / self.chance)
             if self.chance == 100:
-                heal =random.randrange(1, self.maxHP - self.hp)
+                heal = random.randrange(1, self.maxHP - self.hp)
                 self.hp += heal
                 self.chance = 0
-                return  f"Поздравляю. \nВы увеличили свое здоровье на {heal}"
+                return f"Поздравляю. \nВы увеличили свое здоровье на {heal}"
 
             elif self.chance > 0 and self.chance != 100 and can_u_heal >= 0.5:
                 heal = random.randrange(1, self.maxHP - self.hp)
@@ -124,27 +124,33 @@ class Player(object):
                 self.chance = 0
                 return f"Поздравляю. \nВы увеличили свое здоровье на {heal}"
             elif can_u_heal < 0:
-                damage = random.randrange(1, self.maxHP-self.hp)
+                damage = random.randrange(1, self.maxHP - self.hp)
                 self.hp -= damage
-                self.chance += random.randrange(5,21)
-                return  f"О НЕЕЕЕТ. \nТы получил мать его критический промах. \nШприц порвал твою артерию. \nТы полуил {damage} урона"
+                self.chance += random.randrange(5, 21)
+                return f"О НЕЕЕЕТ. \nТы получил мать его критический промах. \nШприц порвал твою артерию. \nТы полуил {damage} урона"
 
             else:
-                self.chance += random.randrange(1,21)
-                return  'Сегодня не твой день. \nЯ думаю в следущий раз повезет'
+                self.chance += random.randrange(1, 21)
+                return 'Сегодня не твой день. \nЯ думаю в следущий раз повезет'
 
         elif self.hp == self.maxHP:
-            return  'Вы и так здоровы. \nЖаль, но вы пропускаете ход'
+            return 'Вы и так здоровы. \nЖаль, но вы пропускаете ход'
 
     def get_stat(self, whatUwant):
-        if whatUwant == 'name':
-            return self.name
-        elif whatUwant == 'age':
-            return self.age
-        elif whatUwant == 'country':
-            return self.country
-        elif whatUwant == 'hp':
-            return self.hp
+        match whatUwant:
+            case 'name':
+                return self.name
+            case 'age':
+                return self.age
+            case 'country':
+                return self.country
+            case 'hp':
+                return self.hp
+            case 'race':
+                return  self.race
+            case _:
+                return  'ХАХАХАХАХАХХА БАГ БАГ, НУ ВЫ ВИДЕЛИ. ТУТ БАГ'
+
 
     def shutdown(self):
         kill = self.maxHP * 10
@@ -152,14 +158,15 @@ class Player(object):
         self.hp -= kill
         return 'Never gonna give u up'
 
-random.shuffle(en_conf.name)
 
-random.shuffle(en_conf.race)
+random.shuffle(En_conf.name)
 
-random.shuffle(en_conf.country)
+random.shuffle(En_conf.race)
 
-monster = Enemy(random.choice(en_conf.name), en_conf.age,
-                random.choice(en_conf.race), en_conf.hp, en_conf.monster_maxAttack, random.choice(en_conf.country))
+random.shuffle(En_conf.country)
+
+monster = Enemy(random.choice(En_conf.name), En_conf.age,
+                random.choice(En_conf.race), En_conf.hp, En_conf.monster_maxAttack, random.choice(En_conf.country))
 
 player_name = input('Как вас зовут странник - ')
 player_age = int(input('Сколько лет вашему герою - '))
@@ -187,7 +194,10 @@ step = 0
 
 
 def start_battle(main_hero, enemy_battle):
-    global  last_action_player, last_action_monster, first_move, second_move, step
+    global last_action_player, last_action_monster, first_move, second_move, step, end
+
+    end = 'Maybe'
+
     while True:
 
         enemy_move = random.randrange(2)
@@ -203,7 +213,7 @@ def start_battle(main_hero, enemy_battle):
 
         if enemy_battle.dead() is True or main_hero.dead():
             os.system('cls' if os.name == 'nt' else 'clear')
-            print('Игра окончена')
+            print('\nИгра окончена')
 
             if enemy_battle.dead() is True and main_hero.dead():
                 print(
@@ -211,13 +221,18 @@ def start_battle(main_hero, enemy_battle):
                 break
             elif enemy_battle.dead() is True and not main_hero.dead():
                 print(
-                    f"Вы вернулись с поля битвы, оставив {enemy_battle.get_stat('name')} мертвым на поле сражения. \nДальнейшая судьба {main_hero.get_stat('name')} неизвестна")
+                    f"\n\nВы вернулись с поля битвы, оставив {enemy_battle.get_stat('name')} мертвым на поле сражения. \nДальнейшая судьба {main_hero.get_stat('name')} неизвестна")
                 break
             elif not enemy_battle.dead() and main_hero.dead() is True:
                 print(
-                    f"Вы сегодня не вернетесь домой. Вы проиграли.\n{enemy_battle.get_stat('name')} ушел в неизвестность, дальнейшая его судьба неизвестна. \nЧто будет с {main_hero.get_stat('name')} никто не знает")
+                    f"\n\nВы сегодня не вернетесь домой. Вы проиграли.\n{enemy_battle.get_stat('name')} ушел в неизвестность, дальнейшая его судьба неизвестна. \nЧто будет с {main_hero.get_stat('name')} никто не знает")
                 break
 
+        elif end == 'Never gonna give u up':
+            os.system('cls' if os.name == 'nt' else 'clear')
+            print(f"\nВы сдались как вы могли. \nВы поступили подло и дали {enemy_battle.get_stat('name')} возможность прославиться на вашем поражении."
+                  f"\nПускай теперь все знают {main_hero.get_stat('name')}, как {main_hero.get_stat('race')} проиграл рассе {enemy_battle.get_stat('race')}")
+            break
         elif step == 0 and first_move == 'Enemy':
             if enemy_battle.get_stat('hp') == enemy_battle.get_stat('maxHP') or enemy_move == 1:
                 last_action_monster = main_hero.get_damage(enemy_battle.attack())
@@ -234,7 +249,7 @@ def start_battle(main_hero, enemy_battle):
             elif action == 2:
                 last_action_player = main_hero.health()
             elif action == 3:
-                main_hero.shutdown()
+                end =  main_hero.shutdown()
             step += 1
         elif step == 1 and second_move == 'Player':
             action = int(input('\n1)Атака \n2)Вылечиться \n3)Сдаться \nЧто делаем - '))
@@ -244,7 +259,7 @@ def start_battle(main_hero, enemy_battle):
             elif action == 2:
                 last_action_player = main_hero.health()
             elif action == 3:
-                main_hero.shutdown()
+                end = main_hero.shutdown()
             step = 0
 
         if step == 1 and second_move == 'Enemy':
@@ -255,6 +270,7 @@ def start_battle(main_hero, enemy_battle):
 
             step = 0
 
-start_battle (player, monster)
+
+start_battle(player, monster)
 
 input('\nНажмите enter для завершения игры')
